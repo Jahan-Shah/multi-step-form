29fd71a20063b6dc7888fbed2bed5509f54ccd93
Shah Jahan <shahjahansarfraz3973@gmail.com>
2023-07-04 14:22:21 +0500

    tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,27 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: 'hsl(206, 94%, 87%)',
+          200: 'hsl(228, 100%, 84%)',
+          300: 'hsl(243, 100%, 62%)',
+          400: 'hsl(213, 96%, 18%)'
+        },
+        error: 'hsl(354, 84%, 57%)',
+        secondary: {
+          100: 'hsl(0, 0%, 100%)',
+          200: 'hsl(231, 100%, 99%)',
+          300: 'hsl(217, 100%, 97%)',
+          400: 'hsl(229, 24%, 87%)',
+          500: 'hsl(231, 11%, 63%)',
+        }
+      },
+      fontFamily: {
+        ubuntu: "'Ubuntu', sans-serif",
+      }
+    },
   },
   plugins: [],
 }
