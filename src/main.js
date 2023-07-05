6eea6d36ca1869664cca191b993c5779576de7ad
Shah Jahan <shahjahansarfraz3973@gmail.com>
2023-07-05 17:10:38 +0500

    added pinia and router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
+import { createPinia } from 'pinia'
 import { createApp } from 'vue'
+
 import App from './App.vue'
+import router from './router'
+
 import './reset.css'
 import './style.css'
 
+const pinia = createPinia();
 const app = createApp(App)
 
+app.use(pinia).use(router)
 app.mount('#app')
