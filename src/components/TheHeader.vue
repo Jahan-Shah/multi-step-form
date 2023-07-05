73065586062b7a58c4e1996a801726119b2362b6
Shah Jahan <shahjahansarfraz3973@gmail.com>
2023-07-05 19:57:48 +0500

    header style done

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,34 @@
+<template>
+  <header>
+    <nav>
+      <ul class="flex justify-center gap-3.5">
+        <li>
+          <RouterLink
+            active-class="link__active"
+            class="link"
+            to="/personal-info"
+            >1</RouterLink
+          >
+        </li>
+        <li>
+          <RouterLink active-class="link__active" class="link" to="/plan"
+            >2</RouterLink
+          >
+        </li>
+        <li>
+          <RouterLink active-class="link__active" class="link" to="/add-ons"
+            >3</RouterLink
+          >
+        </li>
+        <li>
+          <RouterLink
+            active-class="link__active"
+            class="link"
+            to="/finishing-up"
+            >4</RouterLink
+          >
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
