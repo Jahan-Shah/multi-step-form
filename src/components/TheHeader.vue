4e644a944621ad04742708eac6770fcaeb7e977b
Shah Jahan <shahjahansarfraz3973@gmail.com>
2023-07-25 18:40:25 +0500

    changed styling

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,50 +1,52 @@
 <template>
   <header>
     <nav>
-      <ul class="flex justify-center gap-3.5 sm:flex-col sm:gap-8">
-        <li class="flex gap-3.5">
-          <RouterLink
-            active-class="link__active"
-            class="link"
-            to="/personal-info"
-            >1</RouterLink
-          >
+      <div class="flex justify-center gap-3.5 sm:flex-col sm:gap-8">
+        <RouterLink
+          class="flex w-max gap-3.5"
+          active-class="link__active"
+          to="/personal-info"
+        >
+          <div class="link">1</div>
           <div class="text-sm uppercase max-sm:hidden">
             <p class="text-xs text-primary-200">Step 1</p>
             <h3 class="font-medium text-secondary-100">Your Info</h3>
           </div>
-        </li>
-        <li class="flex gap-3.5">
-          <RouterLink active-class="link__active" class="link" to="/plan"
-            >2</RouterLink
-          >
+        </RouterLink>
+        <RouterLink
+          class="flex w-max gap-3.5"
+          active-class="link__active"
+          to="/plan"
+        >
+          <div class="link">2</div>
           <div class="text-sm uppercase max-sm:hidden">
-            <p class="text-xs text-primary-200">Step 2</p>
-            <h3 class="font-medium text-secondary-100">Select Plan</h3>
+            <p class="text-xs text-primary-200">Step 1</p>
+            <h3 class="font-medium text-secondary-100">Your Info</h3>
           </div>
-        </li>
-        <li class="flex gap-3.5">
-          <RouterLink active-class="link__active" class="link" to="/add-ons"
-            >3</RouterLink
-          >
+        </RouterLink>
+        <RouterLink
+          class="flex w-max gap-3.5"
+          active-class="link__active"
+          to="/add-ons"
+        >
+          <div class="link">3</div>
           <div class="text-sm uppercase max-sm:hidden">
-            <p class="text-xs text-primary-200">Step 3</p>
-            <h3 class="font-medium text-secondary-100">Add-Ons</h3>
+            <p class="text-xs text-primary-200">Step 1</p>
+            <h3 class="font-medium text-secondary-100">Your Info</h3>
           </div>
-        </li>
-        <li class="flex gap-3.5">
-          <RouterLink
-            active-class="link__active"
-            class="link"
-            to="/finishing-up"
-            >4</RouterLink
-          >
+        </RouterLink>
+        <RouterLink
+          class="flex w-max gap-3.5"
+          active-class="link__active"
+          to="finishing-up"
+        >
+          <div class="link">4</div>
           <div class="text-sm uppercase max-sm:hidden">
-            <p class="text-xs text-primary-200">Step 4</p>
-            <h3 class="font-medium text-secondary-100">Summary</h3>
+            <p class="text-xs text-primary-200">Step 1</p>
+            <h3 class="font-medium text-secondary-100">Your Info</h3>
           </div>
-        </li>
-      </ul>
+        </RouterLink>
+      </div>
     </nav>
   </header>
 </template>
