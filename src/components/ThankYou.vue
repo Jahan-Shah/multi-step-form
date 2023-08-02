92365ac73bc589efdfd4593cbcc072fb28b662fa
Shah Jahan <shahjahansarfraz3973@gmail.com>
2023-08-02 13:39:49 +0500

    added thank you component

---
+++ src/components/ThankYou.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="max-sm:base__card w-80 h-full flex justify-center input__flex">
+    <div class="flex items-center text-center flex-col gap-1.5">
+      <img
+        class="h-14 w-14"
+        src="../assets/images/icon-thank-you.svg"
+        alt="check"
+      />
+      <h2 class="text-2xl font-bold">Thank you!</h2>
+      <p class="text-sm">
+        Thanks for confirming your subscription! We hope you have fun using our
+        platform. If you ever need support, please feel free to email us at
+        support@toremgaming.com.
+      </p>
+    </div>
+  </div>
+</template>
