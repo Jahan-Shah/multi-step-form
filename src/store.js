aa87bbaefe44f7cc5a94ed5b967acbb6a8c91f6f
Shah Jahan <shahjahansarfraz3973@gmail.com>
2023-08-02 13:40:59 +0500

    submit form function

--- src/store.js
+++ src/store.js
@@ -1,19 +1,54 @@
 import { defineStore } from "pinia";
-import { ref, watch } from "vue";
+import { computed, ref, watch } from "vue";
+import { useRouter } from "vue-router";
 
 export const useStore = defineStore("store", () => {
+  const router = useRouter();
   const name = ref("");
   const email = ref("");
   const phoneNumber = ref();
   const plan = ref("");
-  const planPrice = ref();
+  const planPrice = ref(0);
   const duration = ref("Monthly");
   const addOns = ref([]);
   const addOnPrice = ref([]);
 
+  const emailReg = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+  const phoneReg = /^\+?[0-9()-\s]+$/
+  const isFormValid = ref(true);
+  const isFormSubmitted = ref(false);
+
+  const isEmailValid = computed(() => {
+    return !emailReg.test(email.value)
+  })
+
+  const isNameEmpty = computed(() => {
+    return name.value === '';
+  })
+
+  const isPhoneNumberValid = computed(() => {
+    return !phoneReg.test(phoneNumber.value);
+  })
+
+  const isPlanSelected = computed(() => {
+    return plan.value !== "";
+  })
+
   const totalAddOn = () => {
     addOnPrice.value = addOns.value.map(addOn => addOn.price);
     addOnPrice.value = addOnPrice.value.reduce((acc, curr) => acc + curr, 0);
+  }
+
+  const submitForm = () => {
+    isFormValid.value = true;
+    if (isEmailValid.value || isNameEmpty.value || isPhoneNumberValid.value || !isPlanSelected.value) {
+      isFormValid.value = false
+      return;
+    } else {
+      isFormValid.value = true;
+      isFormSubmitted.value = true;
+      router.push('/thank-you')
+    }
   }
 
   watch(addOns, totalAddOn)
@@ -42,5 +77,12 @@ export const useStore = defineStore("store", () => {
     addOns,
     addOnPrice,
     setDuration,
+    isEmailValid,
+    isNameEmpty,
+    isPhoneNumberValid,
+    isPlanSelected,
+    isFormValid,
+    submitForm,
+    isFormSubmitted
   }
 });
